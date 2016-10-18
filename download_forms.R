0ea1882023ee1328d43bafaa4557e2431aaa9ef1
orhahog <orhahog@gmail.com>
2016-10-17 20:38:57 -0400

    Function do not use any local files
    This function download all master files from SEC website and saves .csv
    with information for SC13D\G filings into a working directory.
    Then script downloads all filings into into a sql database.

--- download_forms.R
+++ download_forms.R
@@ -1,78 +1,61 @@
 require(data.table)
 require(Hmisc)
-setwd(wd)
+#setwd(wd)
 ###########################################
-####### prepare SEC master file ###########
+####### construct SEC master file ########
 ###########################################
-load( , file = "./CRSP_COMP/sec_master_1994_2015.rda")
-setkey(sec_master,cik)
-sec <- sec_master[grep("SC 13(D|G)", form_type)]
-sec[, date := as.Date(date)]
-sec[, file := gsub(".*/", "", link)]
-sec[, file := gsub("\\s","", file)]
-sec[, year := year(date)]
-
-
-
-sec[, folder := gsub(".txt", "", file)]
-sec[, folder := gsub("-","", folder)]
-sec[, address := paste0("https://www.sec.gov/Archives/edgar/data/",
-                                cik, "/", folder, "/",file)]
-write.csv(sec, "sec.csv", row.names = F)
-rm(sec_master)
-
-
-for(year in sort(unique(sec$year)))
+annual.master.file <- function(year)
 {
-  dir.create(file.path("./Master Files/", year), showWarnings = FALSE)
-}
-
-###########################################
-####### download all SC13 forms ###########
-#######     ~20 hours    ##################
-###########################################
-
-for(look_year in 1994:2015)
-{
-  print(Sys.time())
-  print(look_year)
-  dir <- paste0("./Master Files/", look_year)
-  setwd(dir)
-  year_table <- sec[year == look_year]
-  year_table[, address := gsub("\\s","", address)]
-  N <- length(year_table$date)
-  print(N)
-  for(i in 1:N)
+  require(data.table)
+  master_total <- NULL
+  for(i in 1:4)
   {
-    try(download.file(year_table$address[i], year_table$file[i], quiet = T))
+    name <- paste0("ftp://ftp.sec.gov/edgar/full-index/",year,"/QTR",i,"/master.idx")
+    print(sprintf("Downloading master file for quarter %d of year %s...", i, year))
+    master <- readLines(url(name))
+    master <- master[grep("SC 13(D|G)", master)]
+    master_table <- read.table(textConnection(master), sep = "|")
+    rm(master)
+    colnames(master_table) <- c("cik", "name", "type", "date", "link")
+    master_table <- as.data.table(master_table)
+    master_table[, link := paste0("https://www.sec.gov/Archives/", link)]
+    master_table[, file := gsub(".*/", "", link)]
+    master_total <- rbind(master_total, master_table)
+    closeAllConnections()
+  }
+  return(master_total)
+}
+###########################################
+#### download all files into temp dir  ####
+###########################################
+dwnld.files <- function(master)
+{
+  dir.create("temp_dir")
+  for(i in 1:length(master$file))
+  {
+    try(download.file(master$link[i], paste0("./temp_dir/",master$file[i]), quiet = T))
   }
 }
-
 ###########################################
 ####### put all forms in SQdatabase #######
 ###########################################
-
-library(DBI)
-library(RSQLite)
-together <- function(x)
+put.files.in.sql <- function(dbname)
 {
-  return(paste(x, collapse = "\n"))
-}
-setwd("./Comptele_Submission_Files/")
-
-for(year in 1994:2015)
-{
-  start_time <- Sys.time()
-  print(year)
-  dbname <- paste0(year, ".sqlite")
+  library(DBI)
+  library(RSQLite)
+  together <- function(x)
+  {
+    return(paste(x, collapse = "\n"))
+  }
+  
   con = dbConnect(SQLite(), dbname=dbname)
   dbSendQuery(conn=con,
               "CREATE TABLE compsubm
-            (FILENAME TEXT, COMLSUBFILE TEXT)")
-  path <- paste0("./Master Files/", year, "/")
+              (FILENAME TEXT, COMLSUBFILE TEXT)")
+  path <- paste0("./temp_dir/")
   files <- list.files(path)
   n <- length(files)
-  step <- 100
+  step <- 500
   for(i in 1:(n %/% step + 1))
   {
     start <- 1 + (i-1)*step
@@ -87,8 +70,16 @@ for(year in 1994:2015)
     dbWriteTable(conn=con, name = "compsubm", data, append = T)
   }
   dbDisconnect(con)
-  end_time <- Sys.time()
-  print(end_time - start_time)
+  unlink("temp_dir", recursive = T)
 }
+
+for(year in 2001:2003)
+{
+  master <- annual.master.file(year)
+  write.csv(master, paste0("master_", year, ".csv"))
+  dwnld.files(master)
+  put.files.in.sql(paste0(year, ".sqlite"))
+}
+
 
 
